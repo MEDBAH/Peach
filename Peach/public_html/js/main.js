d08619eb975f0f00dc2e90cdbbe1d820c7bfeb2b
Mr-Fox <41732102+Mr-F0x@users.noreply.github.com>
2018-09-06 11:12:35 +0200

    ini

--- Peach/public_html/js/main.js
+++ Peach/public_html/js/main.js
@@ -1,30 +1,4 @@
 $(document).ready(function () {
     $(".wallet-container").slick({
-        dots: false,
-        infinite: true,
-        slidesToShow: 1,
-        centerMode: true,
-        variableWidth: true,
-        centerPadding: '60px',
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 3
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 1
-                }
-            }
-        ]
     });
 });
