9b4aba6048b89fcb02623ddaf0133690763cb71f
Mr-Fox <41732102+Mr-F0x@users.noreply.github.com>
2018-09-06 16:58:13 +0200

    initiation

--- Peach/public_html/js/main.js
+++ Peach/public_html/js/main.js
@@ -1,9 +1,2 @@
 $(document).ready(function () {
-    $(".wallet-container").slick({
-        dots: false,
-        infinite: true,
-        slidesToShow: 1,
-        centerMode: true,
-        variableWidth: true
-    });
 });
