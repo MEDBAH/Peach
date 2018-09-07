3937a0269c4801fd3bd15eca37c2a0c6c32fa568
Mr-Fox <41732102+Mr-F0x@users.noreply.github.com>
2018-09-07 13:53:09 +0200

    inita

--- Peach/public_html/js/main.js
+++ Peach/public_html/js/main.js
@@ -1,2 +1,20 @@
 $(document).ready(function () {
+//    $(".wallet-container").flickity({
+//        freeScroll: true,
+//        contain: false,
+//        dragThreshold: 10,
+//        freeScrollFriction: 0.10,
+//        wrapAround: true,
+//        prevNextButtons: false,
+//        pageDots: false
+//    });
+    $("#owl-demo").owlCarousel({
+        loop: true,
+        center: true,
+        items: 1.5,
+        dots: false,
+        nav: false,
+        dragEndSpeed: 500,
+        margin: 30
+    });
 });
