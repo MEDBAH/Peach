ea0de27668ea2c6a537b7e5b6e085120ad7b9f2c
Mr-Fox <41732102+Mr-F0x@users.noreply.github.com>
2018-09-10 08:16:21 +0200

    initiaion

--- Peach/public_html/js/main.js
+++ Peach/public_html/js/main.js
@@ -1,6 +1,21 @@
 $(document).ready(function () {
-    $(window).scroll(function () {
-        $("body.home:before").css("top", "-70px " + ($(this).scrollTop() / 2) + "px");
+
+
+
+    $(window).on("scroll", function () {
+        if ($(window).scrollTop() > 10) {
+            $("body").addClass("scrolled");
+            $(".navigator .page-title span").addClass("scrolled");
+            $("body").css("margin-top", "1rem");
+            $(".navigator .menu-icon div").css("background-color", "black");
+            $("main.home .wallets-container .wallet").css("margin-top", "3rem");
+        } else {
+            $("body").removeClass("scrolled");
+            $(".navigator .page-title span").removeClass("scrolled");
+            $("body").css("margin-top", "0");
+            $(".navigator .menu-icon div").css("background-color", "white");
+            $("main.home .wallets-container .wallet").css("margin-top", "4.5rem");
+        }
     });
 
 
@@ -11,6 +26,18 @@ $(document).ready(function () {
         dots: false,
         nav: false,
         dragEndSpeed: 300,
-        margin: 20
+        margin: 20,
+        responsive: {
+            // breakpoint from 480 up
+            480: {
+                items: 1.8,
+                margin: 20
+            },
+            // breakpoint from 768 up
+            768: {
+                items: 1.99,
+                margin: 10
+            }
+        }
     });
 });
